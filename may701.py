147180c6b0df4e61ef35b2b1b5d790bc618f4bd5
PRASHANTH.G <messimanju8181@gmail.com>
2022-05-07 17:50:25 +0530

    may7

---
+++ may701.py
@@ -0,0 +1,59 @@
+num = int(input("Enter any number: "))
+flag = num%2
+if flag == 0:
+    print(num, "is an even number")
+elif flag == 1:
+    print(num, "is an odd number")
+else:
+    print("Error, Invalid input")
+
+
+'''
+num= int(input("Enter any number"))
+flag = num%2
+if flag ==0:
+    print(num,"is an even number")
+elif flag == 1:
+    print(num,"is an odd number")
+else: 
+    print("Enter invilid input")
+'''
+
+num = int(input("Enter value any number"))
+if num % 2 == 0:
+    print(num,"is an even number")
+
+
+num = int(input("Enter any value"))
+if num % 2 ==0:
+    print(num, "any number")
+
+num = int(input("enter the valued number"))
+if num % 2 ==0:
+    print(num, "odd")
+
+def evenodd(p):
+    if (p % 2 ==0):
+        print("even")
+    else:
+        print("odd")
+
+def name1(x):
+    print(x.upper)
+    print(name1("manju"))
+
+def name1(x):
+    print(x.upper())
+    print(name1("manju"))
+
+def oddeven(p):
+    if (p % 2 ==0):
+        print("even")
+    else:
+        print("odd")
+
+
+
+
+
+
